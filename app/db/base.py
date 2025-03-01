c47a4cd8418f51257adb8eddbc56a25bf1516616
blackantdxb1 <Arshia@petmagix.com>
2025-03-01 18:36:27 +0330

    Your commit message

--- app/db/base.py
+++ app/db/base.py
@@ -2,3 +2,6 @@
 from app.db.base_class import Base
 from app.models.user import User
 from app.models.pet import Pet, PetMetrics
+from app.models.habit import Habit
+from app.models.metric import Metric
+from app.models.reward import Reward
